350bc22a93097fb398e66c46a7182182e46ef345
Rick Neves <ricknevesbc@icloud.com>
2024-01-15 09:52:46 -0300

    fix(react-query): change lib to react-query

--- web/src/providers/ReactQueryProvider.tsx
+++ web/src/providers/ReactQueryProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode } from 'react'
+import { QueryClientProvider } from 'react-query'
 
 import { queryClient } from '@/lib/reactQuery'
 
